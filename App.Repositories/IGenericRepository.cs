363dcd817f11493496388105084f38b8a4edf1d3
emirk <catiemirkaan@gmail.com>
2026-03-25 20:16:46 +0300

    feat(repositories): add generic repository base

---
+++ App.Repositories/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+    internal interface IGenericRepository<T> where T: class // Tüm entity'ler için ortak CRUD işlemlerini tek tek yazmak yerine,
+                                                            // generic bir repository sözleşmesi tanımlıyoruz.
+    {
+        IQueryable<T> GetAll(); //Sorguya sonradan filtreleme, sıralama ve sayfalama ekleyebilmek için IQueryable döndürüyoruz
+                                //Dbdeki ilgili entitylerin
+                                //tamamını sorgulanabilir bir şekilde döndürmeye yarıyor.
+
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+        ValueTask<T?> GetByIdAsync(int id); //Task da kullanabilirdik
+
+        ValueTask AddAsync(T entity);
+
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
