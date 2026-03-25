10d11ab75b564be88c1637078ba766b23126bc11
emirk <catiemirkaan@gmail.com>
2026-03-25 03:41:44 +0300

    feat(repositories): add sql server dbcontext registration

---
+++ App.Repositories/ConnectionsStringOption.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace App.Repositories
+{
+    public class ConnectionsStringOption
+    {
+        public const string Key = "ConnectionStrings";
+        public string SqlServer { get; set; } = null!;
+
+    }
+}
