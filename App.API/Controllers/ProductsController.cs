12934a0072b4e1bcaf5e576b85306e2f0a4801ff
emirk <catiemirkaan@gmail.com>
2026-04-04 08:10:46 +0300

    add custom base controller and products endpoints

---
+++ App.API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using App.Services.Products;
+using Azure.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace App.API.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController] bunlar artık CustomBaseController'dan geliyor
+    public class ProductsController(IProductService productService) : CustomBaseController
+    {
+        [HttpGet]
+        public async Task <IActionResult> GetAll()=> CreateActionResult(await productService.GetAllListAsync());
+        //burda(yukarıda) productResult'a 
+        //serviceResult geldiği için products     
+        //yerine serviceResult diye isimlendirdim.
+       [HttpGet]
+        public async Task<IActionResult> GetById(int id)=>CreateActionResult(await productService.GetByIdAsync(id));
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductRequest request) =>CreateActionResult(await productService.CreateAsync(request));
+        [HttpPut]
+        public async Task<IActionResult> Update(int id, UpdateProductRequest request) => CreateActionResult(await productService.UpdateAsync(id, request));
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)=>CreateActionResult(await productService.DeleteAsync(id));
+        
+    }
+}
+ 
